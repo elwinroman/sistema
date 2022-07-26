cae95b6dcf5165f2858efe3e98fecf940bf2c918
elwinroman <elwinromanv@gmail.com>
2022-07-26 17:43:15 -0500

    Agrega tabla cargo, historial_cargo

--- config/bd.sql
+++ config/bd.sql
@@ -8,13 +8,41 @@ CREATE TABLE usuarios (
   CONSTRAINT uq_username UNIQUE (username)
 ) ENGINE=INNODB;
 
--- Tabla OFICINAS
-CREATE TABLE oficinas (
+-- Tabla OFICINA
+CREATE TABLE oficina (
   id INT NOT NULL AUTO_INCREMENT,
-  oficina_id INT NULL,
-  nombre VARCHAR(100) NOT NULL,
+  nombre VARCHAR(60) NOT NULL,
   observacion VARCHAR(200) NULL
-  PRIMARY KEY (id),
+  oficina_id INT NULL,
+  PRIMARY KEY(id),
   CONSTRAINT uq_nombre UNIQUE(nombre),
-  CONSTRAINT fk_oficina FOREIGN KEY (oficina_id) REFERENCES oficinas (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT fk_oficina_oficinaid FOREIGN KEY(oficina_id) 
+    REFERENCES oficina(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=INNODB;
+
+-- Tabla CARGO
+CREATE TABLE cargo (
+  id INT NOT NULL AUTO_INCREMENT,
+  situacion CHAR(1) NOT NULL,
+  presupuesto BOOLEAN NOT NULL,
+  observacion VARCHAR(200) NOT NULL,
+  PRIMARY KEY(id)
+)
+
+-- Tabla HISTORIAL-CARGO
+CREATE TABLE historial_cargo (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(60) NOT NULL,
+  nro_plaza CHAR(3) NOT NULL,
+  clasificacion VARCHAR(8) NOT NULL,
+  codigo VARCHAR(10) DEFAULT NULL,
+  ordenanza VARCHAR(50) NOT NULL,
+  fecha_ordenanza DATE NOT NULL,
+  oficina_id INT NOT NULL,
+  cargo_id INT NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_cargo_oficinaid FOREIGN KEY(oficina_id) 
+    REFERENCES oficina(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT fk_cargo_cargoid FOREIGN KEY(cargo_id) 
+    REFERENCES cargo(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+)
