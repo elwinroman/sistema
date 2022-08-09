2e1f9fc2dc6e214681bd7549f73bfe3bfb9d5c3b
elwinroman <elwinromanv@gmail.com>
2022-08-09 00:15:06 -0500

    Añade un cambio en el historial de cargo

--- controllers/CargoController.php
+++ controllers/CargoController.php
@@ -137,8 +137,59 @@ class CargoController extends ControllerBase {
         $this->view->render('cargo/details');
     }
 
+    // Añade cambios en el historial
+    public function addchanges() {
+        if(!$this->existsPOST(['nombre','nro-plaza','clasificacion','codigo','ordenanza',
+                            'fecha-ordenanza','oficina-jefe']) && isset($_GET['id'])) {
+            $this->redirect('error');
+        }
+
+        // tratamiento de los datos
+        $cargo_data = array(
+            'nombre'        => $this->util->limpiar_cadena($_POST['nombre']),
+            'nro_plaza'     => $this->util->reduce_multiple_space($_POST['nro-plaza']),
+            'clasificacion' => $this->util->limpiar_cadena($_POST['clasificacion']),
+            'codigo'        => $this->util->reduce_multiple_space($_POST['codigo']),
+            'ordenanza'     => $this->util->limpiar_cadena($_POST['ordenanza']),
+            'fecha_ordenanza' => $_POST['fecha-ordenanza'],
+            'cargo_id'      => $_GET['id'],
+            'oficina_id'    => null
+        );
+
+        if(isset($_POST['checkbox']) && isset($_POST['suboficina'])) {
+            $cargo_data['oficina_id'] = $_POST['suboficina'];
+        } else
+            $cargo_data['oficina_id'] = $_POST['oficina-jefe'];
+
+        // validación de datos
+        if(!$this->validate($cargo_data)) {
+            $this->redirect('cargo/details&id='.$_GET['id']);
+            return;
+        }
+
+        // asignación de datos
+        $cargo = $this->loadModel('cargo');     // este modelo incluye a las tablas cargo e historial_cargo
+
+        $cargo->setNombre($cargo_data['nombre']);
+        $cargo->setNroPlaza($cargo_data['nro_plaza']);
+        $cargo->setClasificacion($cargo_data['clasificacion']);
+        $cargo->setCodigo($cargo_data['codigo']);
+        $cargo->setOrdenanza($cargo_data['ordenanza']);
+        $cargo->setFechaOrdenanza($cargo_data['fecha_ordenanza']);
+        $cargo->setOficinaId($cargo_data['oficina_id']);
+        $cargo->setCargoId($cargo_data['cargo_id']);
+
+        // insertar en la base de datos
+        if($cargo->insertChanges()) {
+            $this->redirect('cargo/details&id='.$_GET['id']);
+            return;
+        } else  {
+            $this->redirect('cargo/details&id='.$_GET['id']);
+            return;
+        }
+    }
     /**
-     * Valida los datos al crear o editar un cargo
+     * Valida los datos al crear, añadir cambios o editar un cargo
      * @param  Array $data   Default: random var, lo importante es que reciba un array
      * @return Bool
      */
@@ -156,14 +207,20 @@ class CargoController extends ControllerBase {
             return false;
         if(strlen($data['codigo']) > 0 && !filter_var($data['codigo'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^[0-9]{1,10}$/']]))
             return false;
-        if(!filter_var($data['situacion'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(o|O|p|P)$/']]))
-            return false;
+        if(array_key_exists('situacion', $data)) { 
+            if(!filter_var($data['situacion'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(o|O|p|P)$/']]))
+                return false;
+        }
         if(!filter_var($data['ordenanza'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^([A-Za-zÀ-ÿ0-9,;(){}[\]*+¿?!¡:._-]\s?){1,50}$/']]))
             return false;
         if(!$this->util->validateDate($data['fecha_ordenanza']))
             return false;
         if(!filter_var($data['oficina_id'], FILTER_VALIDATE_INT))
             return false;
+        if(array_key_exists('cargo_id', $data)) {
+            if(!filter_var($data['cargo_id'], FILTER_VALIDATE_INT))
+                return false;
+        }
 
         return true;
     }
---
+++ views/cargo/add-changes.php
@@ -0,0 +1,69 @@
+<form id="form-cargo" class="form-add" autocomplete="on" action="<?=URL_BASE?>cargo/addchanges&id=<?=$_GET['id']?>" method="POST">
+    <div class="row g-0 gap-ow-15">
+        <div class="datos-cargo col-md-6">
+            
+            <!-- Campo nombre -->
+            <div class="form-field-ow">
+                <label for="nombre" class="bold-ow">Nombre</label>
+                <input class="input-ow input-height-ow" type="text" name="nombre" pattern="\s*([A-Za-zÀ-ÿ,.-]\s*)+" maxlength="60" required autofocus>
+            </div>
+
+            <div class="row g-0 gap-ow-5">
+                <!-- Campo nro_plaza -->
+                <div class="form-field-ow col">
+                    <label for="nro-plaza" class="bold-ow">Nro plaza</label>
+                    <input class="input-ow input-height-ow" type="text" name="nro-plaza" pattern="\s*[0-9]{3}\s*" maxlength="6" required>
+                </div>
+
+                <!-- Campo clasificacion -->
+                <div class="form-field-ow col">
+                    <label for="clasificacion" class="bold-ow">Clasificacion</label>
+                    <input class="input-ow input-height-ow" type="text" name="clasificacion" pattern="\s*[A-Za-z-]+\s*" maxlength="8" required>
+                </div>
+            </div>
+
+            <!-- Campo codigo -->
+            <div class="form-field-ow col">
+                <label for="codigo" class="bold-ow">Código</label>
+                <input class="input-ow input-height-ow" type="text" name="codigo" pattern="\s*[0-9]+\s*" maxlength="10">
+            </div>
+
+            <!-- Campo ordenanza -->
+            <div class="form-field-ow">
+                <label for="ordenanza" class="bold-ow">Ordenanza</label>
+                <input class="input-ow input-height-ow" type="text" name="ordenanza" pattern="\s*([A-Za-zÀ-ÿ0-9,;(){}[\]*+¿?!¡:._-]\s*)+" maxlength="50" required>
+            </div>
+
+            <div class="form-field-ow" style="width: 50%">
+                <label for="fecha-ordenanza">Fecha de ordenanza</label>
+                <input class="input-ow input-height-ow" name="fecha-ordenanza" type="date" required>
+            </div>
+
+        </div>
+
+        <div class="col-md-6">
+            
+            <!-- Campo oficina-jefe -->
+            <div class="form-field-ow">
+                <label for="oficina-jefe" class="bold-ow">Órgano</label><br>
+                <select name="oficina-jefe" class="input-ow input-height-ow" required></select>
+            </div>
+
+            <div class="form-field-ow">
+                <input type="checkbox" class="form-check-input" name="checkbox" value="active">
+                <label for="checkbox" class="">Habilitar Suboficinas</label>
+            </div>
+
+            <!-- Campo suboficina -->
+            <div class="form-field-ow">
+                <label for="suboficina" class="bold-ow">Unidad orgánica</label><br>
+                <select name="suboficina" class="input-ow input-height-ow" disabled="true"></select>
+            </div>
+
+        </div>
+    </div>
+
+    <div>
+        <button type="submit" class="btn-ow btn-ow-blue">Añadir</button>
+    </div>
+</form>
